7f8f601169ae2c57818d6f6c4080a2e03f673e57
Sergio Martins <iamsergio@gmail.com>
2012-02-20 05:49:43 +0000

    Require version 0.15.0 of libgit2.
    It doesn't have a stable ABI yet, so we must use a fixed version.

--- cmake/modules/FindLibgit2.cmake
+++ cmake/modules/FindLibgit2.cmake
@@ -1,23 +1,61 @@
-if(Libgit2_INCLUDE_DIR AND Libgit2_LIBRARY)
-  # Already in cache, be silent
-  set(Libgit2_FIND_QUIETLY TRUE)
-endif()
+# Find Libgit2
+#
+# This module defines
+#  Libgit2_FOUND - whether the qsjon library was found
+#  Libgit2_LIBRARIES - the qjson library
+#  Libgit2_INCLUDE_DIR - the include path of the qjson library
+#
+# Based on FindQJSON.cmake from the akonadi-facebook resource by Thomas McGuire and Pino Toscano
+#
 
-FIND_PATH(Libgit2_INCLUDE_DIR git2.h libgit2/include)
-FIND_LIBRARY(Libgit2_LIBRARY NAMES git2 PATHS libgit2)
+if (Libgit2_INCLUDE_DIR AND Libgit2_LIBRARIES AND Libgit2_VERSION)
 
-if(Libgit2_INCLUDE_DIR AND Libgit2_LIBRARY)
-  set(Libgit2_FOUND TRUE)
-endif()
+  # Already in cache
+  set (Libgit2_FOUND TRUE)
 
-if(Libgit2_FOUND)
-  if(NOT Libgit2_FIND_QUIETLY)
-    message(STATUS "Found libgit2: ${Libgit2_LIBRARY}")
-  endif()
-else()
-  if(Libgit2_FIND_REQUIRED AND NOT Libgit2_FIND_QUIETLY)
-    message(FATAL_ERROR "Could NOT find libgit2 library")
-  endif()
-endif()
+else (Libgit2_INCLUDE_DIR AND Libgit2_LIBRARIES AND Libgit2_VERSION)
 
-mark_as_advanced(Libgit2_LIBRARY Libgit2_INCLUDE_DIR)
+  if (NOT WIN32)
+    set (_pc_libgit2_string "libgit2")
+    if (Libgit2_FIND_VERSION_EXACT)
+      set (_pc_libgit2_string "${_pc_libgit2_string}=")
+    else (Libgit2_FIND_VERSION_EXACT)
+      set (_pc_libgit2_string "${_pc_libgit2_string}>=")
+    endif (Libgit2_FIND_VERSION_EXACT)
+    if (Libgit2_FIND_VERSION_COUNT GREATER 0)
+      set (_pc_libgit2_string "${_pc_libgit2_string}${Libgit2_FIND_VERSION}")
+    else (Libgit2_FIND_VERSION_COUNT GREATER 0)
+      set (_pc_libgit2_string "libgit2")
+    endif (Libgit2_FIND_VERSION_COUNT GREATER 0)
+    # use pkg-config to get the values of Libgit2_INCLUDE_DIRS
+    # and Libgit2_LIBRARY_DIRS to add as hints to the find commands.
+    include (FindPkgConfig)
+    pkg_check_modules (PC_Libgit2 ${_pc_libgit2_string})
+    set (Libgit2_VERSION "${PC_Libgit2_VERSION}")
+  endif (NOT WIN32)
+
+  find_library (Libgit2_LIBRARIES
+    NAMES
+    git2
+    PATHS
+    ${PC_Libgit2_LIBRARY_DIRS}
+    ${LIB_INSTALL_DIR}
+    ${KDE4_LIB_DIR}
+  )
+
+  find_path (Libgit2_INCLUDE_DIR
+    NAMES
+    git2.h
+    PATHS
+    ${PC_Libgit2_INCLUDE_DIRS}
+    ${INCLUDE_INSTALL_DIR}
+    ${KDE4_INCLUDE_DIR}
+  )
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(Libgit2
+                                    REQUIRED_VARS Libgit2_LIBRARIES Libgit2_INCLUDE_DIR Libgit2_VERSION
+                                    VERSION_VAR Libgit2_VERSION
+  )
+
+endif (Libgit2_INCLUDE_DIR AND Libgit2_LIBRARIES AND Libgit2_VERSION)
